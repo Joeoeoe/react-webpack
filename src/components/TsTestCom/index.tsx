77fc3baaf6eb9750d89ca673bf5cf167c5172dd4
lihanzhou <lihanzhou@tencent.com>
2020-06-28 14:45:53 +0800

    添加ts对css模块的声明再global.d.ts中，并把全局声明添加在package.json，同时tsconfig添加esModuleInterop字段，import时可以少写点。如import * as React from "react"变为import React from "react"

--- src/components/TsTestCom/index.tsx
+++ src/components/TsTestCom/index.tsx
@@ -1,10 +1,10 @@
-import  * as React from 'react';
-import * as styles from './index.module.css';
+import  React from 'react';
+import styles from './index.module.css';
 
 interface HelloProps { compiler: string; framework: string; }
 
-
 const TsTestCom: React.FC<HelloProps> = (props: HelloProps) => {
+    
     return (
         <h1 className={styles["test"]}>
             Hello from {props.compiler} and {props.framework}!
--- webpack.common.js
+++ webpack.common.js
@@ -181,7 +181,7 @@ module.exports = {
 
   resolve: {
     // 支持缩写
-    extensions: ['.js', '.jsx', ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     // 别名
     alias: {
       '@': path.join(__dirname, 'src'), // "@表示src目录，即\src"
