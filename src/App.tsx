2301cbe853d015befe24c369363c4ae0bb2033f1
Joeoeoe <695993410@qq.com>
2020-08-22 10:53:28 +0800

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { hot } from 'react-hot-loader/root'; // 热替换
 import React from 'react';
 import './App.css';
-
 import TemplateCom from './components/TestCom';
 import TsTestCom from './components/TsCom';
 import PureTsCom from './components/PureTsCom';
 
 function App() {
   const a = 1,
-                          b = 3;
+    b = 3;
   const arr = ['ABCDEFG'];
   return (
     <div>
